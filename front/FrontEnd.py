fb4323250b6b0e19b1376e3efb42a5b64f804c1e
Nick Pydyn <pydyn17@gmail.com>
2018-11-18 14:50:48 -0500

    display directory

--- front/FrontEnd.py
+++ front/FrontEnd.py
@@ -2,26 +2,36 @@ import curses
 import curses.textpad
 
 import sys
-import os
 
 from CLIexceptions.CLI_Audio_Exception import CLI_Audio_File_Exception
 from CLIexceptions.CLI_Audio_Exception import CLI_Audio_Screen_Size_Exception
+
+import os
+from os import listdir
 
 class FrontEnd:
 
     def __init__(self, player):
         self.player = player
-        self.player.play(sys.argv[1])
         curses.wrapper(self.menu)
 
     def menu(self, args):
         self.stdscr = curses.initscr()
         self.stdscr.border()
         self.stdscr.addstr(0,0, "cli-audio",curses.A_REVERSE)
-        self.stdscr.addstr(5,10, "c - Change current song")
-        self.stdscr.addstr(6,10, "p - Play/Pause")
-        self.stdscr.addstr(7,10, "l - Library")
-        self.stdscr.addstr(9,10, "ESC - Quit")
+        self.stdscr.addstr(5,45, "c - Change current song")
+        self.stdscr.addstr(6,45, "p - Play/Pause")
+        self.stdscr.addstr(7,45, "l - Play library on playlist")
+        self.stdscr.addstr(9,45, "ESC - Quit")
+
+        lib = "/home/pydynn/343/PythonMusic/library"
+        liblist = os.listdir(lib)
+        y = 6
+        for mus in liblist:
+            self.stdscr.addstr(y,10,mus)
+            y = y+1
+        self.stdscr.addstr(5,10,"Files currently in library:")
+
         self.updateSong()
         self.stdscr.refresh()
         while True:
@@ -35,15 +45,17 @@ class FrontEnd:
                 self.updateSong()
                 self.stdscr.touchwin()
                 self.stdscr.refresh()
-    
+            elif c == ord('l'):
+                self.updateSong()
+                 
     def updateSong(self):
-        self.stdscr.addstr(15,10, "                                        ")
-        self.stdscr.addstr(15,10, "Now playing: " + self.player.getCurrentSong())
+        self.stdscr.addstr(15,45, "                                        ")
+        self.stdscr.addstr(15,45, "Now playing: " + self.player.getCurrentSong())
 
     def changeSong(self):
         changeWindow = curses.newwin(5, 40, 5, 50)
         changeWindow.border()
-        changeWindow.addstr(0,0, "What is the file path?", curses.A_REVERSE)
+        changeWindow.addstr(0,0, "What is the song's file name?", curses.A_REVERSE)
         self.stdscr.refresh()
         curses.echo()
         path = changeWindow.getstr(1,1, 30)
