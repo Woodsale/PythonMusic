ab42cd2a9d876b0d8b2a90af1713d531261bf89d
Woodsale <woodsale@mail.gvsu.edu>
2018-11-19 10:32:51 -0500

    Update CLI_Audio_Exception.py

--- CLIexceptions/CLI_Audio_Exception.py
+++ CLIexceptions/CLI_Audio_Exception.py
@@ -14,3 +14,8 @@ class CLI_Audio_Screen_Size_Exception(CLI_Audio_Exception):
     def __init__(self, message, errors):
         super().__init__(message)
         self.errors = errors
+
+class CLI_NULL_Audio_File_Exception(CLI_Audio_Exception):
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
